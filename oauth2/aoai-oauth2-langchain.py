3605554383a1d3979b88d94dab54edcc551b9c28
deuch <sdechoux@microsoft.com>
2024-05-07 12:47:00 +0200

    add langchain sample

--- oauth2/aoai-oauth2-langchain.py
+++ oauth2/aoai-oauth2-langchain.py
@@ -1,7 +1,7 @@
 import os
 import httpx
 import json
-from openai import AzureOpenAI
+from langchain_openai import AzureOpenAI
 from dotenv import load_dotenv
 from httpx_auth import OAuth2ClientCredentials
 
@@ -16,31 +16,17 @@ AZURE_OPENAI_DEPLOYMENT_NAME=os.environ.get('AZURE_OPENAI_DEPLOYMENT_NAME')
 AZURE_OPENAI_MODEL_NAME=os.environ.get('AZURE_OPENAI_MODEL_NAME')
 OPENAI_API_VERSION=os.environ.get('OPENAI_API_VERSION')
 
+
 # verify=False for debug purpose with proxy
 # Will use an handler to store token in memory and refresh
 oauth2_httpxclient=httpx.Client()
 auth=OAuth2ClientCredentials(OIDC_ENDPOINT, client_id=OIDC_CLIENT_ID, client_secret=OIDC_CLIENT_SECRET, scope=OIDC_SCOPE, client=oauth2_httpxclient)
 
-client = AzureOpenAI(
-      api_version=OPENAI_API_VERSION,
-      azure_endpoint=AZURE_OPENAI_ENDPOINT,
-      api_key="FAKE_KEY",
-      http_client=httpx.Client(auth=auth)
-  )
-
-
 # Loop to check if the token is retrieved from cache
 
-for i in range(1, 4):
+llm=AzureOpenAI(deployment_name=AZURE_OPENAI_DEPLOYMENT_NAME,model_name=AZURE_OPENAI_MODEL_NAME,http_client=httpx.Client(auth=auth))
+question="How do I output all files in a directory using Python?"
 
-  completion = client.chat.completions.create(
-      model=AZURE_OPENAI_DEPLOYMENT_NAME,  # e.g. gpt-35-instant
-      messages=[
-          {
-              "role": "user",
-              "content": "How do I output all files in a directory using Python?",
-          },
-      ]
-  )
-  print(completion.choices[0].message.content)
+print(llm.invoke(question))
+print(llm)
 
